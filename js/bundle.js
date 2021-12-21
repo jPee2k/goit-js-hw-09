a8ddb29d8583235fec1dffbbedd98bdd26866344
jPee2k <jPee.dev@gmail.com>
2021-12-21 09:26:34 +0000

    Deploying to gh-pages from @ jPee2k/goit-js-hw-09@59122f1172ddb210eaa2f612e29cd3d051deafdb 🚀

---
+++ js/bundle.js
@@ -0,0 +1,2 @@
+(()=>{"use strict";document.addEventListener("DOMContentLoaded",(()=>{console.log("color switcher"),console.log("timer"),console.log("promise")}))})();
+//# sourceMappingURL=bundle.js.map
